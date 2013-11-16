04f326df922c595476e6389f0463677be61b7cb5
Tejas Dinkar <tejas@gja.in>
2013-11-16 15:51:41 +0530

    Almost done

--- python_module/librupy.c
+++ python_module/librupy.c
@@ -1,18 +1,28 @@
 #include "Python.h"
 
-static PyObject *run_fibonacci_in_ruby(PyObject *self, PyObject *number)
+void rupy_start_ruby_vm();
+void rupy_require_file(const char *file);
+long rupy_eval(const char *string);
+
+static PyObject *python_rupy_require(PyObject *self, PyObject *file)
 {
-    long value = PyInt_AsLong(number);
-    printf("%li", value);
+    rupy_require_file(PyString_AsString(file));
     return Py_True;
 }
 
+static PyObject *python_rupy_eval(PyObject *self, PyObject *string)
+{
+    return PyInt_FromLong(rupy_eval(PyString_AsString(string)));
+}
+
 static PyMethodDef module_functions[] = {
-    { "factorial", run_fibonacci_in_ruby, METH_O, "Calculate factorial" },
+    { "require", python_rupy_require, METH_O, "Require A File" },
+    { "eval", python_rupy_eval, METH_O, "Evaluate Ruby Code that returns an int" },
     { NULL }
 };
 
 void initlibrupy(void)
 {
+    rupy_start_ruby_vm();
     Py_InitModule3("librupy", module_functions, "A ruby module for python.");
 }
