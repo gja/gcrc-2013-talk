85017139afe3dcb4742c7e53de5e1f9a324d6487
Tejas Dinkar <tejas@gja.in>
2013-11-16 23:46:06 +0530

    Working with Ruby 1.8. On to 1.9

--- python_module/test_librupy_backend.c
+++ python_module/test_librupy_backend.c
@@ -9,7 +9,9 @@ long rupy_eval(const char *string);
 int main(int argc, char **argv)
 {
 	rupy_start_ruby_vm();
-	rupy_require_file("./factorial");
+	rupy_eval("$: << '.'");
+	rupy_require_file("factorial");
+	rupy_eval("def factorial(n); 10; end");
 	printf("%li\n", rupy_eval("factorial 5"));
 	return 0;
 }
